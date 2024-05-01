cda1d665bf46b961e2a21253ec1f14a83a14c62a
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-05-01 17:00:17 +0200

    build: configure webpack

---
+++ eCommerce-App/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "./src/index.ts"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+      	    filename: "index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+              {
+                from: path.resolve(__dirname, "src/img"),
+                to: path.resolve(__dirname, "dist/img"),
+              },
+            ],
+        }),
+    ],
+    devServer: {
+        open: true,
+        host: "localhost",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            img: path.join(__dirname, "src", "img"),
+        },
+    },
+};
