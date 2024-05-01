421d686f7539665d8fd89bd5ad580cbf1ed729dc
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-05-01 17:04:17 +0200

    feat: configure typescript setup

--- eCommerce-App/webpack.config.js
+++ eCommerce-App/webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
+            {
+                test: /\.ts$/i,
+                use: "ts-loader",
+            },
         ],
     },
     resolve: {
