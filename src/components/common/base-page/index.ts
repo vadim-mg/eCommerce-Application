b8beb22ff8c90fbcdff683e6a6070d35b59620af
vadim-mg <vadikm1981@yandex.ru>
2024-05-07 19:59:02 +0500

    feat: implement base page

---
+++ src/components/common/base-page/index.ts
@@ -0,0 +1,23 @@
+import Container from '@Src/components/ui/container';
+import classes from './style.module.scss';
+
+type PageProps = {
+  containerTag: keyof HTMLElementTagNameMap;
+  title: string;
+};
+
+export default class BasePage {
+  protected container: Container;
+
+  constructor(props: PageProps) {
+    this.container = new Container({ tag: props.containerTag });
+  }
+
+  // this method clear document.body and append container created by constructor
+  render() {
+    document.body.innerHTML = '';
+    document.body.classList.remove(...document.body.classList);
+    document.body.classList.add(classes.page);
+    document.body.append(this.container.node);
+  }
+}
