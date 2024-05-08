11d2715f15d3b7634b41d18857b5a7fecb991e71
vadim-mg <vadikm1981@yandex.ru>
2024-05-08 08:45:07 +0500

    feat: imlement hidden-example page

---
+++ src/pages/hidden-example/index.ts
@@ -0,0 +1,79 @@
+import BaseElement from '@Src/components/common/base-element';
+import BasePage from '@Src/components/common/base-page';
+import tag from '@Src/components/common/tag';
+
+// imports pictures for example
+import imageBoard from '@Img/board-game-example-image.webp';
+import imageSvg from '@Assets/icons/favicon.svg';
+
+// import api for example
+import categoriesApi from '@Src/api/categories';
+
+import classes from './style.module.scss';
+
+export default class HiddenExamplePage extends BasePage {
+  #content!: BaseElement<HTMLDivElement>;
+
+  constructor() {
+    super({ containerTag: 'div', title: 'Hidden Example page' });
+    this.#createContent();
+    this.#showContent();
+    this.#showCategories();
+  }
+
+  #createContent = () => {
+    this.#content = new BaseElement<HTMLDivElement>(
+      {
+        tag: 'main',
+        class: classes.hiddenExample,
+      },
+
+      // variant1 use class BaseElement<T>
+      new BaseElement<HTMLDivElement>({ tag: 'h1', text: 'Hello' }),
+      new BaseElement<HTMLParagraphElement>({ tag: 'p', text: 'This is hidden example page' }),
+      new BaseElement<HTMLOListElement>(
+        { tag: 'ol' },
+        new BaseElement({ tag: 'li', text: 'one', class: classes.listItem }),
+        new BaseElement<HTMLLIElement>({ tag: 'li', text: 'two' }),
+        new BaseElement<HTMLLIElement>({ tag: 'li', text: 'three', title: 'dfdfd' }),
+      ),
+
+      // more short variant, use function Tag. Result is equivalent!
+      tag({ tag: 'h1', text: 'Hello' }),
+      tag({ tag: 'p', text: 'This is hidden example page' }),
+      tag(
+        { tag: 'ol' },
+        tag({ tag: 'li', text: 'one', class: classes.listItem }),
+        tag({ tag: 'li', text: 'two' }),
+        tag({ tag: 'li', text: 'three', title: 'title props' }),
+      ),
+
+      // the same what was in component 'example' on branch 'test'
+      tag({ tag: 'div', text: 'example', class: classes.example }),
+      tag({ tag: 'div', text: 'example2 - google font', class: classes.example3 }),
+      tag<HTMLImageElement>({ tag: 'img', src: imageBoard, class: classes.example2 }),
+      tag<HTMLImageElement>({ tag: 'img', src: imageSvg, class: classes.imageExample }),
+    );
+  };
+
+  // exapmle of using SDK for get categories by API
+  #showCategories = () => {
+    categoriesApi
+      .getCategories()
+      .then((resp) => {
+        console.log('resp.body.results');
+        console.log(resp.body.results);
+        this.#content.node.append(
+          tag({ tag: 'ul', text: `categories` }).node,
+          ...resp.body.results.map(
+            (category) => tag({ tag: 'li', text: `${category.name['en-GB']}` }).node,
+          ),
+        );
+      })
+      .catch(console.error);
+  };
+
+  #showContent = () => {
+    this.container.node.append(this.#content.node);
+  };
+}
