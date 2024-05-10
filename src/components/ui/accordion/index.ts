f7a18695dc87de173ebb6af0a40c37291b3d63bc
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2024-05-10 22:17:35 +0300

    Feat/implement accordion component (#91)
    * feat: add logic for accordion
    
    * fix: change accordion logic for smooth opening
    
    * feat: add styles for accordion
    
    * fix: change styles for checkbox
    
    * fix: added the ability to scroll content with a smaller container height

---
+++ src/components/ui/accordion/index.ts
@@ -0,0 +1,81 @@
+import BaseElement from '@Src/components/common/base-element';
+import Container from '../container';
+
+import classes from './style.module.scss';
+
+export enum AccordionState {
+  OPEN,
+  CLOSED,
+}
+
+export default class Accordion extends BaseElement<HTMLDivElement> {
+  contentContainer!: Container;
+
+  fullHeight!: number;
+
+  isOpen: boolean = false;
+
+  constructor(title: string, state: AccordionState, ...children: BaseElement<HTMLElement>[]) {
+    super({ tag: 'div', class: [classes.accordion] });
+    this.#createAccordion(state, title, children);
+  }
+
+  #createAccordion(state: AccordionState, title: string, children: BaseElement<HTMLElement>[]) {
+    this.#addHeader(title);
+    this.#addContent(state, children);
+  }
+
+  #addHeader = (title: string) => {
+    const header = new BaseElement({ tag: 'div', class: [classes.header] });
+    const titleText = new BaseElement({ tag: 'div', class: [classes.title], textContent: title });
+    header.node.append(titleText.node);
+    this.node.append(header.node);
+
+    header.node.addEventListener('click', () => {
+      this.#toggleAccordion();
+    });
+  };
+
+  #addContent = (state: AccordionState, children: BaseElement<HTMLElement>[]) => {
+    this.contentContainer = new Container({ tag: 'div', class: [classes.container] }, ...children);
+    this.node.append(this.contentContainer.node);
+    // element rendering delay
+    setTimeout(() => {
+      this.fullHeight = this.contentContainer.node.offsetHeight;
+      console.log(`Высота: ${this.fullHeight}px`);
+
+      this.#setDefaultState(state);
+    }, 1);
+  };
+
+  #setDefaultState = (state: AccordionState) => {
+    if (state === AccordionState.OPEN) {
+      this.isOpen = true;
+      this.node.classList.add(classes.open);
+      this.#showContent();
+    } else {
+      this.isOpen = false;
+      this.node.classList.add(classes.closed);
+      this.#hiddenContent();
+    }
+  };
+
+  #toggleAccordion = () => {
+    this.isOpen = !this.isOpen;
+    this.node.classList.toggle(classes.open);
+    this.node.classList.toggle(classes.closed);
+    if (this.isOpen) {
+      this.#showContent();
+    } else {
+      this.#hiddenContent();
+    }
+  };
+
+  #showContent = () => {
+    this.contentContainer.node.style.height = `${this.fullHeight}px`;
+  };
+
+  #hiddenContent = () => {
+    this.contentContainer.node.style.height = '0';
+  };
+}
