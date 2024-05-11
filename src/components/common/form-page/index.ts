c80fd914c70675f9100203357c93f682f2a16838
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-05-11 18:27:15 +0200

    feat: add styles to form-page

--- src/components/common/form-page/index.ts
+++ src/components/common/form-page/index.ts
@@ -1,6 +1,7 @@
 import Container from '@Src/components/ui/container';
 import Wrapper from '@Src/components/ui/wrapper';
 import logoSvgLight from '@Assets/icons/logo-light.svg';
+import errorSvg from '@Assets/icons/error.svg';
 import BasePage from '../base-page';
 import classes from './style.module.scss';
 import BaseElement from '../base-element';
@@ -31,18 +32,23 @@ export default class FormPage extends BasePage {
   }
 
   createBasicContent = (errorText: string) => {
-    this.logoComponent = new BaseElement<HTMLImageElement>({
-      tag: 'img',
-      src: logoSvgLight,
-    });
-    this.errorComponent = new BaseElement<HTMLDivElement>({ tag: 'div', text: errorText });
+    this.logoComponent = new BaseElement<HTMLImageElement>({ tag: 'img', src: logoSvgLight });
+    this.createErrorComponent(errorText);
     this.formWrapper = new BaseElement<HTMLDivElement>({ tag: 'div' });
-    this.modalContainer = new BaseElement<HTMLDivElement>({ tag: 'div' });
+    this.modalContainer = new BaseElement<HTMLDivElement>({ tag: 'div', class: classes.modalContainer });
 
     this.modalContainer.node.append(this.logoComponent.node);
     this.modalContainer.node.append(this.errorComponent.node);
     this.modalContainer.node.append(this.formWrapper.node);
     return this.modalContainer;
+  };
+
+  createErrorComponent = (errorText: string) => {
+    const errorIcon = new BaseElement<HTMLImageElement>({ tag: 'img', class: classes.errorIcon, src: errorSvg });
+    const errorTextElement = new BaseElement<HTMLParagraphElement>({ tag: 'p', text: errorText });
+    this.errorComponent = new BaseElement<HTMLDivElement>({ tag: 'div', class: classes.errorComponent });
+    this.errorComponent.node.append(errorIcon.node);
+    this.errorComponent.node.append(errorTextElement.node);
   }
 
   render = () => {
