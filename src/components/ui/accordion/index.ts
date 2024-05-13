93913aaf0e5f1edd2e96451ae60ccd4fbf7f5226
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2024-05-13 15:25:29 +0300

    Fix/rss ecomm 2 00 13 accordion component mobile support (#93)
    * fix: change overflow in accordion
    
    * fix: Add an event listener to support accordion on mobile devices
    
    * fix: add an event listener to support accordion on mobile devices
    
    * fix: add recalculation of the height of the open accordion after changing the screen size
    
    * fix: replace the Container with a BaseElement
    
    * fix: remove Container from component Accordion
    
    * fix: format

--- src/components/ui/accordion/index.ts
+++ src/components/ui/accordion/index.ts
@@ -1,5 +1,4 @@
 import BaseElement from '@Src/components/common/base-element';
-import Container from '../container';
 
 import classes from './style.module.scss';
 
@@ -9,9 +8,9 @@ export enum AccordionState {
 }
 
 export default class Accordion extends BaseElement<HTMLDivElement> {
-  contentContainer!: Container;
+  contentContainer!: BaseElement<HTMLDivElement>;
 
-  contentWrapper!: Container;
+  contentWrapper!: BaseElement<HTMLDivElement>;
 
   fullHeight!: number;
 
