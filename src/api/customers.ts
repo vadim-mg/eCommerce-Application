db25b782b48a8cd174c1d25e94375c5643d0dab7
vadim-mg <vadikm1981@yandex.ru>
2024-05-13 17:38:31 +0500

    feat: add signUp signIn for customers

--- src/api/customers.ts
+++ src/api/customers.ts
@@ -1,15 +1,23 @@
-import { CustomerDraft } from "@commercetools/platform-sdk";
-import apiRoot from "./api-root";
+import { CustomerDraft, CustomerSignin } from '@commercetools/platform-sdk';
+import apiRoot from './api-root';
 
 // https://docs.commercetools.com/sdk/sdk-example-code
+// https://docs.commercetools.com/api/projects/customers#authenticate-sign-in-customer
 
-const createCustomer = (customer: CustomerDraft) => apiRoot
-  .customers()
-  .post(
-    {
-      // The CustomerDraft is the object within the body
-      body: customer
+const signUp = (customer: CustomerDraft) =>
+  apiRoot
+    .customers()
+    .post({
+      body: customer,
     })
-  .execute();
+    .execute();
 
-export default createCustomer;
+const signIn = (customer: CustomerSignin) =>
+  apiRoot
+    .login()
+    .post({
+      body: customer,
+    })
+    .execute();
+
+export default { signIn, signUp };
