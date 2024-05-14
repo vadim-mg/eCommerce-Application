86884007a9241090dc8237209c149df61f923d0c
vadim-mg <vadikm1981@yandex.ru>
2024-05-14 23:55:57 +0500

    refactor: header component

--- src/components/ui/header/index.ts
+++ src/components/ui/header/index.ts
@@ -19,131 +19,124 @@ const routeToLogin = () => Router.getInstance().route(AppRoutes.LOGIN);
 const routeToSignup = () => Router.getInstance().route(AppRoutes.SIGNUP);
 
 export default class Header extends BaseElement<HTMLElement> {
-  userActionsWrapper!: BaseElement<HTMLDivElement>;
+  #mainNavItems = [AppRoutes.MAIN, AppRoutes.CATALOGUE, AppRoutes.ABOUT];
 
-  buttonContainer!: BaseElement<HTMLDivElement>;
+  #sidebarNavItems = [AppRoutes.LOGIN, AppRoutes.SIGNUP, AppRoutes.PROFILE, AppRoutes.LOGOUT];
 
-  navigationList!: BaseElement<HTMLUListElement>;
+  #burgerButton: BaseElement<HTMLDivElement>;
 
-  navigationListBurger!: BaseElement<HTMLUListElement>;
-
-  burgerButton!: BaseElement<HTMLDivElement>;
-
-  hamburgerSidebar!: HamburgerSidebar;
-
-  #mainNavItems: AppRoutes[];
-
-  #sidebarNavItems: AppRoutes[];
+  #hamburgerSidebar: HamburgerSidebar;
 
   constructor(props: HeaderProps) {
     super({ tag: 'header', class: classes.header, ...props });
-    this.#mainNavItems = [
-      AppRoutes.MAIN,
-      AppRoutes.CATALOGUE,
-      AppRoutes.ABOUT
-    ];
-    this.#sidebarNavItems = [
-      AppRoutes.LOGIN,
-      AppRoutes.SIGNUP,
-      AppRoutes.PROFILE,
-      AppRoutes.LOGOUT
-    ];
-    this.#createContent();
+    this.#burgerButton = Header.#createBurgerButton();
+    this.#hamburgerSidebar = this.#createBurgerMenu();
+    this.#appendContent();
+    this.#addEventListeners();
   }
 
-  #createContent = () => {
-    this.createButtonContainer();
-
-    this.createBurgerMenu();
-
-    this.createBurgerButton();
-
-    this.node.append(this.#createLogoNavigationWrapper().node);
-    this.node.append(Header.#createCartElement().node);
-    this.node.append(this.buttonContainer.node);
-    this.node.append(this.burgerButton.node);
-    this.node.append(this.hamburgerSidebar.node);
-    this.burgerButton.node.addEventListener('click', this.hamburgerSidebar.openSidebar);
+  #addEventListeners = () => {
+    this.#burgerButton.node.addEventListener('click', this.#hamburgerSidebar.openSidebar);
   };
 
-  #createLogoNavigationWrapper = () => {
-    const navigationList = this.#mainNavItems.map((route) => new BaseElement<HTMLLIElement>(
-      {
-        tag: 'li',
-        class: Router.isCurrentPath(route) ? classes.current : '',
-      },
-      new Link({
-        href: route,
-        text: ROUTES[route].name,
-        class: classes.navLink,
-      })
-    ));
+  #appendContent = () => {
+    this.node.append(
+      this.#hamburgerSidebar.node,
+      this.#createLogoNavigationWrapper().node,
+      Header.#createUserActionsWrapper().node,
+      this.#burgerButton.node,
+    );
+  };
 
-    return new BaseElement<HTMLDivElement>(
+  #createLogoNavigationWrapper = () =>
+    new BaseElement<HTMLDivElement>(
       { tag: 'div', class: classes.logoNavigationWrapper },
-      new BaseElement<HTMLImageElement>(
-        {
-          tag: 'img',
-          class: classes.logoIcon,
-          src: logoSvg,
-        }),
+      new BaseElement<HTMLImageElement>({
+        tag: 'img',
+        class: classes.logoIcon,
+        src: logoSvg,
+      }),
       new BaseElement<HTMLElement>(
         {
           tag: 'nav',
           class: '',
         },
-        new BaseElement<HTMLUListElement>({ tag: 'ul', class: classes.navigationList }, ...navigationList)
+        new BaseElement<HTMLUListElement>(
+          { tag: 'ul', class: classes.navigationList },
+          ...this.#mainNavItems.map(Header.#createListItem),
+        ),
       ),
     );
-  };
 
-  createBurgerButton = () => {
-    this.burgerButton = new BaseElement<HTMLDivElement>({
-      tag: 'div',
-      class: classes.burgerButton,
-    });
+  #createBurgerMenu = () =>
+    new HamburgerSidebar(
+      { class: classes.mobileMenu },
+      new BaseElement<HTMLUListElement>(
+        { tag: 'ul', class: classes.navigationList },
+        ...[...this.#mainNavItems, ...this.#sidebarNavItems]
+          .filter(Header.#filterListItem)
+          .map(Header.#createListItem),
+      ),
+    );
 
-    let i = 0;
-    while (i < 3) {
-      const burgerBtnLine = new BaseElement<HTMLDivElement>({
-        tag: 'div',
-        class: classes.burgerBtnLine,
-      });
-      this.burgerButton.node.append(burgerBtnLine.node);
-      i += 1;
-    }
-  };
-
-  static #createCartElement = () => new BaseElement<HTMLDivElement>(
-    {
-      tag: 'div',
-      class: classes.userActionsWrapper,
-    },
+  static #createBurgerButton = () =>
     new BaseElement<HTMLDivElement>(
       {
         tag: 'div',
-        class: classes.basket,
+        class: classes.burgerButton,
       },
-      new BaseElement<HTMLImageElement>({
-        tag: 'img',
-        class: classes.basketIcon,
-        src: basketSvg,
-      })
-    )
-  );
+      ...Array(3)
+        .fill(1)
+        .map(
+          () =>
+            new BaseElement<HTMLDivElement>({
+              tag: 'div',
+              class: classes.burgerBtnLine,
+            }),
+        ),
+    );
 
-  createButtonContainer = () => {
-    this.buttonContainer = new BaseElement<HTMLDivElement>(
+  static #createUserActionsWrapper = () =>
+    new BaseElement<HTMLDivElement>(
+      {
+        tag: 'div',
+        class: classes.userActionsWrapper,
+      },
+      // cart element
+      new Link(
+        { href: AppRoutes.CART, class: classes.basket },
+        new BaseElement<HTMLDivElement>(
+          {
+            tag: 'div',
+            class: classes.basket,
+          },
+          new BaseElement<HTMLImageElement>({
+            tag: 'img',
+            class: classes.basketIcon,
+            src: basketSvg,
+          }),
+        ),
+      ),
+      this.#createButtonContainer(),
+    );
+
+  static #createButtonContainer = () =>
+    new BaseElement<HTMLDivElement>(
       {
         tag: 'div',
         class: classes.buttonContainer,
       },
-      new Button({ text: 'Log in', hidden: State.getInstance().isLoggedIn },
+
+      new Button(
+        { text: 'Log in', hidden: State.getInstance().isLoggedIn },
         ButtonClasses.NORMAL,
-        routeToLogin),
-      new Button({ text: 'Sign up', hidden: State.getInstance().isLoggedIn },
+        routeToLogin,
+      ),
+      new Button(
+        { text: 'Sign up', hidden: State.getInstance().isLoggedIn },
         ButtonClasses.NORMAL,
-        routeToSignup),
+        routeToSignup,
+      ),
       new Link({
         href: AppRoutes.PROFILE,
         class: classes.linkUserIcon,
@@ -153,48 +146,39 @@ export default class Header extends BaseElement<HTMLElement> {
         tag: 'img',
         class: classes.userIcon,
         src: userSvg,
-        hidden: !State.getInstance().isLoggedIn
+        hidden: !State.getInstance().isLoggedIn,
       }),
 
-      new Button({ text: 'Log out', hidden: !State.getInstance().isLoggedIn },
+      new Button(
+        { text: 'Log out', hidden: !State.getInstance().isLoggedIn },
         ButtonClasses.NORMAL,
-        auth.signOut)
-    );
-  };
-
-
-  createBurgerMenu = () => {
-    const navigationListBurger = [...this.#mainNavItems, ...this.#sidebarNavItems]
-      .filter(Header.#filterListItem)
-      .map(Header.#createListItem);
-    this.hamburgerSidebar = new HamburgerSidebar(
-      { class: classes.mobileMenu },
-      new BaseElement<HTMLUListElement>(
-        { tag: 'ul', class: classes.navigationList },
-        ...navigationListBurger
+        auth.signOut,
       ),
     );
-  };
 
   // create list item for route
-  static #createListItem = (route: AppRoutes) => new BaseElement<HTMLLIElement>(
-    {
-      tag: 'li',
-      class: Router.isCurrentPath(route) ? classes.current : '',
-    },
-    new Link({
-      href: route,
-      text: ROUTES[route].name,
-      class: classes.navLink,
-    })
-  );
+  static #createListItem = (route: AppRoutes) =>
+    new BaseElement<HTMLLIElement>(
+      {
+        tag: 'li',
+        class: Router.isCurrentPath(route) ? classes.current : '',
+      },
+      new Link({
+        href: route,
+        text: ROUTES[route].name,
+        class: classes.navLink,
+      }),
+    );
 
   // filter routes which should be shown
   static #filterListItem = (route: AppRoutes) => {
     if (ROUTES[route].visibility === RouteVisibility.onlyAuth && !State.getInstance().isLoggedIn) {
       return false;
     }
-    if (ROUTES[route].visibility === RouteVisibility.onlyNotAuth && State.getInstance().isLoggedIn) {
+    if (
+      ROUTES[route].visibility === RouteVisibility.onlyNotAuth &&
+      State.getInstance().isLoggedIn
+    ) {
       return false;
     }
     return true;
