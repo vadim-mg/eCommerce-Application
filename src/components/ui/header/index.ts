fe7d2beed389fee1fca5bfb90da8e08a284d61e1
vadim-mg <vadikm1981@yandex.ru>
2024-05-14 21:57:58 +0500

    refactor: header component

--- src/components/ui/header/index.ts
+++ src/components/ui/header/index.ts
@@ -5,45 +5,20 @@ import BaseElement, { ElementProps } from '@Src/components/common/base-element';
 import Button, { ButtonClasses } from '@Src/components/ui/button';
 import HamburgerSidebar from '@Src/components/ui/hamburger-sidedar';
 import Link from '@Src/components/ui/link';
+import ROUTES, { AppRoutes, RouteVisibility } from '@Src/router/routes';
 import State from '@Src/state';
 
+import auth from '@Src/controllers/auth';
 import Router from '@Src/router';
-import { AppRoutes } from '@Src/router/routes';
 import classes from './style.module.scss';
 
 type HeaderProps = Omit<ElementProps<HTMLElement>, 'tag'>;
 
-interface StringKeyObject {
-  [key: string]: string;
-}
+const routeToLogin = () => Router.getInstance().route(AppRoutes.LOGIN);
 
-export const LinkPath: StringKeyObject = {
-  LOGIN: 'login',
-  LOGOUT: 'logout',
-  SINGUP: 'registration',
-  REGISTRATION: 'registration',
-  HOME: 'main',
-  CATALOGUE: 'catalogue',
-  ABOUT: 'about',
-  PROFILE: 'profile',
-};
-
-const mainNavItems: StringKeyObject = {
-  HOME: 'Home',
-  CATALOGUE: 'Catalogue',
-  ABOUT: 'About shop',
-};
-
-const sidebarNavItems: StringKeyObject = {
-  LOGIN: 'Login',
-  SINGUP: 'Sing in',
-  PROFILE: 'Profile',
-  LOGOUT: 'Logout',
-};
+const routeToSignup = () => Router.getInstance().route(AppRoutes.SIGNUP);
 
 export default class Header extends BaseElement<HTMLElement> {
-  logoNavigationWrapper!: BaseElement<HTMLDivElement>;
-
   userActionsWrapper!: BaseElement<HTMLDivElement>;
 
   buttonContainer!: BaseElement<HTMLDivElement>;
@@ -56,155 +31,70 @@ export default class Header extends BaseElement<HTMLElement> {
 
   hamburgerSidebar!: HamburgerSidebar;
 
-  loginButton!: Button;
+  #mainNavItems: AppRoutes[];
 
-  signinButton!: Button;
-
-  logoutButton!: Button;
-
-  userProfileIco!: Link;
-
-  loginLink!: Link;
-
-  signinLink!: Link;
-
-  logoutLink!: Link;
-
-  userProfileLink!: Link;
-
-  currentPageLinks!: BaseElement<HTMLLIElement>[];
+  #sidebarNavItems: AppRoutes[];
 
   constructor(props: HeaderProps) {
     super({ tag: 'header', class: classes.header, ...props });
+    this.#mainNavItems = [
+      AppRoutes.MAIN,
+      AppRoutes.CATALOGUE,
+      AppRoutes.ABOUT
+    ];
+    this.#sidebarNavItems = [
+      AppRoutes.LOGIN,
+      AppRoutes.SIGNUP,
+      AppRoutes.PROFILE,
+      AppRoutes.LOGOUT
+    ];
     this.#createContent();
   }
 
   #createContent = () => {
-    this.currentPageLinks = [];
-    this.logoNavigationWrapper = new BaseElement<HTMLDivElement>({
-      tag: 'div',
-      class: classes.logoNavigationWrapper,
-    });
-    this.createLogoNavigationContent();
-
-    this.userActionsWrapper = new BaseElement<HTMLDivElement>({
-      tag: 'div',
-      class: classes.userActionsWrapper,
-    });
-    this.createUserActionsContent();
+    this.createButtonContainer();
 
     this.createBurgerMenu();
+
     this.createBurgerButton();
 
-    this.node.append(this.logoNavigationWrapper.node);
-    this.node.append(this.userActionsWrapper.node);
+    this.node.append(this.#createLogoNavigationWrapper().node);
+    this.node.append(Header.#createCartElement().node);
+    this.node.append(this.buttonContainer.node);
     this.node.append(this.burgerButton.node);
-    this.#changeNavView();
+    this.node.append(this.hamburgerSidebar.node);
+    this.burgerButton.node.addEventListener('click', this.hamburgerSidebar.openSidebar);
   };
 
-  createUserActionsContent = () => {
-    this.createBasketElement();
-    this.createButtonContainer();
-  };
-
-  createBasketElement = () => {
-    const basketIcon = new BaseElement<HTMLImageElement>({
-      tag: 'img',
-      class: classes.basketIcon,
-      src: basketSvg,
-    });
-    const basketElement = new BaseElement<HTMLDivElement>(
+  #createLogoNavigationWrapper = () => {
+    const navigationList = this.#mainNavItems.map((route) => new BaseElement<HTMLLIElement>(
       {
-        tag: 'div',
-        class: classes.basket,
+        tag: 'li',
+        class: Router.isCurrentPath(route) ? classes.current : '',
       },
-      basketIcon,
+      new Link({
+        href: route,
+        text: ROUTES[route].name,
+        class: classes.navLink,
+      })
+    ));
+
+    return new BaseElement<HTMLDivElement>(
+      { tag: 'div', class: classes.logoNavigationWrapper },
+      new BaseElement<HTMLImageElement>(
+        {
+          tag: 'img',
+          class: classes.logoIcon,
+          src: logoSvg,
+        }),
+      new BaseElement<HTMLElement>(
+        {
+          tag: 'nav',
+          class: '',
+        },
+        new BaseElement<HTMLUListElement>({ tag: 'ul', class: classes.navigationList }, ...navigationList)
+      ),
     );
-    this.userActionsWrapper.node.append(basketElement.node);
-  };
-
-  createButtonContainer = () => {
-    this.buttonContainer = new BaseElement<HTMLDivElement>({
-      tag: 'div',
-      class: classes.buttonContainer,
-    });
-    this.loginButton = new Button({ text: 'Log in' }, ButtonClasses.NORMAL, () => {
-      Router.getInstance().route(AppRoutes.LOGIN);
-    });
-    this.signinButton = new Button({ text: 'Sign in' }, ButtonClasses.NORMAL, () => {
-      Router.getInstance().route(AppRoutes.SIGNUP);
-    });
-    this.buttonContainer.node.append(this.loginButton.node);
-    this.buttonContainer.node.append(this.signinButton.node);
-
-    this.userProfileIco = new Link({
-      href: AppRoutes.PROFILE,
-      class: classes.linkUserIcon,
-    });
-    const userProfileIcoSVG = new BaseElement<HTMLImageElement>({
-      tag: 'img',
-      class: classes.userIcon,
-      src: userSvg,
-    });
-    this.userProfileIco.node.append(userProfileIcoSVG.node);
-
-    this.logoutButton = new Button({ text: 'Log out' }, ButtonClasses.NORMAL, () => {
-      // there need a callback for unlogging, now it is testing exemple
-      State.getInstance().isLoggedIn = !State.getInstance().isLoggedIn;
-      Router.getInstance().route(AppRoutes.LOGOUT);
-      // after unlogging change button
-      this.#changeNavView();
-    });
-    this.buttonContainer.node.append(this.userProfileIco.node);
-    this.buttonContainer.node.append(this.logoutButton.node);
-
-    this.userActionsWrapper.node.append(this.buttonContainer.node);
-  };
-
-  #changeNavView() {
-    if (!State.getInstance().isLoggedIn) {
-      this.userProfileIco.node.classList.add(classes.hidden);
-      this.userProfileLink.node.classList.add(classes.hidden);
-      this.logoutButton.node.classList.add(classes.hidden);
-      this.logoutLink.node.classList.add(classes.hidden);
-      if (this.loginButton.node.classList.contains(classes.hidden)) {
-        this.loginButton.node.classList.remove(classes.hidden);
-        this.signinButton.node.classList.remove(classes.hidden);
-        this.loginLink.node.classList.remove(classes.hidden);
-        this.signinLink.node.classList.remove(classes.hidden);
-      }
-    } else {
-      this.loginButton.node.classList.add(classes.hidden);
-      this.loginLink.node.classList.add(classes.hidden);
-      this.signinButton.node.classList.add(classes.hidden);
-      this.signinLink.node.classList.add(classes.hidden);
-      if (this.userProfileIco.node.classList.contains(classes.hidden)) {
-        this.userProfileIco.node.classList.remove(classes.hidden);
-        this.userProfileLink.node.classList.remove(classes.hidden);
-        this.logoutButton.node.classList.remove(classes.hidden);
-        this.logoutLink.node.classList.remove(classes.hidden);
-      }
-    }
-  }
-
-  createLogoNavigationContent = () => {
-    const logoIcon = new BaseElement<HTMLImageElement>({
-      tag: 'img',
-      class: classes.logoIcon,
-      src: logoSvg,
-    });
-
-    this.navigationList = this.#createListLinks(mainNavItems);
-    const navigation = new BaseElement<HTMLElement>(
-      {
-        tag: 'nav',
-        class: '',
-      },
-      this.navigationList,
-    );
-
-    this.logoNavigationWrapper.node.append(logoIcon.node);
-    this.logoNavigationWrapper.node.append(navigation.node);
   };
 
   createBurgerButton = () => {
@@ -222,59 +112,91 @@ export default class Header extends BaseElement<HTMLElement> {
       this.burgerButton.node.append(burgerBtnLine.node);
       i += 1;
     }
-
-    this.burgerButton.node.addEventListener('click', this.hamburgerSidebar.openSidebar);
   };
+
+  static #createCartElement = () => new BaseElement<HTMLDivElement>(
+    {
+      tag: 'div',
+      class: classes.userActionsWrapper,
+    },
+    new BaseElement<HTMLDivElement>(
+      {
+        tag: 'div',
+        class: classes.basket,
+      },
+      new BaseElement<HTMLImageElement>({
+        tag: 'img',
+        class: classes.basketIcon,
+        src: basketSvg,
+      })
+    )
+  );
+
+  createButtonContainer = () => {
+    this.buttonContainer = new BaseElement<HTMLDivElement>(
+      {
+        tag: 'div',
+        class: classes.buttonContainer,
+      },
+      new Button({ text: 'Log in', hidden: State.getInstance().isLoggedIn },
+        ButtonClasses.NORMAL,
+        routeToLogin),
+      new Button({ text: 'Sign up', hidden: State.getInstance().isLoggedIn },
+        ButtonClasses.NORMAL,
+        routeToSignup),
+      new Link({
+        href: AppRoutes.PROFILE,
+        class: classes.linkUserIcon,
+      }),
+
+      new BaseElement<HTMLImageElement>({
+        tag: 'img',
+        class: classes.userIcon,
+        src: userSvg,
+        hidden: !State.getInstance().isLoggedIn
+      }),
+
+      new Button({ text: 'Log out', hidden: !State.getInstance().isLoggedIn },
+        ButtonClasses.NORMAL,
+        auth.signOut)
+    );
+  };
+
 
   createBurgerMenu = () => {
-    const burgerLinksName = { ...mainNavItems, ...sidebarNavItems };
-    const navigationListBurger = this.#createListLinks(burgerLinksName);
+    const navigationListBurger = [...this.#mainNavItems, ...this.#sidebarNavItems]
+      .filter(Header.#filterListItem)
+      .map(Header.#createListItem);
     this.hamburgerSidebar = new HamburgerSidebar(
       { class: classes.mobileMenu },
-      navigationListBurger,
+      new BaseElement<HTMLUListElement>(
+        { tag: 'ul', class: classes.navigationList },
+        ...navigationListBurger
+      ),
     );
-    this.node.append(this.hamburgerSidebar.node);
   };
 
-  #createListLinks = (navList: StringKeyObject): BaseElement<HTMLUListElement> => {
-    const list = new BaseElement<HTMLUListElement>({ tag: 'ul', class: classes.navigationList });
-    const listLinksName = Object.keys(navList);
+  // create list item for route
+  static #createListItem = (route: AppRoutes) => new BaseElement<HTMLLIElement>(
+    {
+      tag: 'li',
+      class: Router.isCurrentPath(route) ? classes.current : '',
+    },
+    new Link({
+      href: route,
+      text: ROUTES[route].name,
+      class: classes.navLink,
+    })
+  );
 
-    listLinksName.forEach((name) => {
-      const listItem = new BaseElement<HTMLLIElement>({
-        tag: 'li',
-        class: Router.isCurrentPath(LinkPath[name]) ? classes.current : '',
-      });
-      const link = new Link({
-        href: LinkPath[name],
-        text: navList[name],
-        class: classes.navLink,
-      });
-      listItem.node.append(link.node);
-      list.node.append(listItem.node);
-
-      if (name === 'LOGIN') {
-        this.loginLink = listItem;
-      }
-      if (name === 'SINGUP') {
-        this.signinLink = listItem;
-      }
-      if (name === 'PROFILE') {
-        this.userProfileLink = listItem;
-      }
-      if (name === 'LOGOUT') {
-        this.logoutLink = listItem;
-        link.node.addEventListener('click', (event: Event) => {
-          event.preventDefault();
-          // there need a callback for unlogging, now it is testing exemple
-          State.getInstance().isLoggedIn = !State.getInstance().isLoggedIn;
-          Router.getInstance().route(AppRoutes.LOGOUT);
-          // after unlogging change button
-          this.#changeNavView();
-        });
-      }
-    });
-
-    return list;
+  // filter routes which should be shown
+  static #filterListItem = (route: AppRoutes) => {
+    if (ROUTES[route].visibility === RouteVisibility.onlyAuth && !State.getInstance().isLoggedIn) {
+      return false;
+    }
+    if (ROUTES[route].visibility === RouteVisibility.onlyNotAuth && State.getInstance().isLoggedIn) {
+      return false;
+    }
+    return true;
   };
 }
