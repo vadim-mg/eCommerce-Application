57e43ac288d01870cec0cc9aac479c59ab16c258
Natalia Repkina <pn.88@mail.ru>
2024-05-14 15:58:40 +0300

    feat: add content for page 404

--- src/pages/not-found/index.ts
+++ src/pages/not-found/index.ts
@@ -1,6 +1,6 @@
 import BaseElement from '@Src/components/common/base-element';
 import ContentPage from '@Src/components/common/content-page';
-import tag from '@Src/components/common/tag';
+import { AppRoutes } from '@Src/router/routes';
 import classes from './style.module.scss';
 
 export default class NotFound extends ContentPage {
@@ -13,12 +13,18 @@ export default class NotFound extends ContentPage {
   }
 
   #createContent = () => {
-    this.#content = tag<HTMLDivElement>(
+    this.#content = new BaseElement<HTMLDivElement>(
       {
         tag: 'main',
         class: classes.notFound,
-      },
-      tag<HTMLHeadingElement>({ tag: 'h1', text: '404' }),);
+      },);
+    const header = new BaseElement<HTMLDivElement>({ tag: 'h1', text: '404', class: classes.h1 });
+    const span = new BaseElement<HTMLDivElement>({ tag: 'span', text: 'Page not found', class: classes.span });
+    header.node.append(span.node);
+    const text = new BaseElement<HTMLDivElement>({ tag: 'div', class: classes.text });
+    text.node.innerHTML = `<p>Your path has led you into the unknown.<br>Roll the dice and <a href=${AppRoutes.MAIN}>start over<a>.</p>`;
+    this.#content.node.append(header.node);
+    this.#content.node.append(text.node);
   };
 
   #showContent = () => {
