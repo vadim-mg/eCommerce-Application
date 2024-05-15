d46388157e4ddd24c6b5515caada75e529394b14
vadim-mg <vadikm1981@yandex.ru>
2024-05-15 14:00:47 +0500

    feat: add storing state in session storage

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,13 +1,18 @@
 import Router from '@Src/router';
 
+const AUTH_STATE = 'CJ_ecapp_2024';
+
+type AuthState = {
+  isLoggedIn: boolean;
+};
 export default class State {
   #isLoggedIn: boolean;
 
   static #instance: State | null;
 
   private constructor() {
-    this.#isLoggedIn = false;
-    // todo save loggedState in session storage
+    const { isLoggedIn } = JSON.parse(sessionStorage.getItem(AUTH_STATE) ?? '{}') as AuthState;
+    this.#isLoggedIn = isLoggedIn ?? false;
   }
 
   // State can be used in any part of App as a singleton
@@ -25,6 +30,12 @@ export default class State {
   set isLoggedIn(value: boolean) {
     this.#isLoggedIn = value;
     console.log(`Current state is: ${this.#isLoggedIn}`);
+    sessionStorage.setItem(
+      AUTH_STATE,
+      JSON.stringify({
+        isLoggedIn: this.#isLoggedIn,
+      }),
+    );
     Router.getInstance().refresh();
   }
 }
