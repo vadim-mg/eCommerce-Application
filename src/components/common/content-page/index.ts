ba4b9908fc35c3f86489f641ed8ff721884e4a0a
Yanina Lysukha <108079995+YanaLysukha@users.noreply.github.com>
2024-05-19 17:23:22 +0200

    116-rss-ecomm-2_00_20 implement banner on the main page (#126)
    * feat: add basic banner content
    
    * feat: add styles to banner
    
    * feat: add adaptive layout
    
    * refactor: remove comments
    
    * refactor: change px to rem
    
    * refactor: replace banner btn code into mew method
    
    * feat: add extra links to the main page
    
    * fix: add changes to banner adaptive layout
    
    * fix: add banner appearance only in the main page
    
    * feat: add changes to promo button
    
    * refactor: refactor code
    
    * feat: add styles to main page links
    
    * feat: change CTP_SCOPES
    
    * feat: add banner btn button type
    
    * feat: add title to banner btn
    
    * refactor: format code

--- src/components/common/content-page/index.ts
+++ src/components/common/content-page/index.ts
@@ -1,8 +1,10 @@
 import Container from '@Src/components/ui/container';
 import Footer from '@Src/components/ui/footer';
 import Header from '@Src/components/ui/header';
+import Banner from '@Src/components/ui/banner';
 import BasePage from '../base-page';
 import classes from './style.module.scss';
+import BaseElement from '../base-element';
 
 type ContentPageProps = {
   title: string;
@@ -14,11 +16,19 @@ export default class ContentPage extends BasePage {
 
   header: Header;
 
+  placeForBanner: BaseElement<HTMLElement>;
+
+  banner!: Banner;
+
   footer: Footer;
 
   constructor(props: ContentPageProps) {
     super({ title: props.title });
     this.header = new Header({});
+    this.placeForBanner = new BaseElement<HTMLElement>({
+      tag: 'div',
+      class: classes.bannerWrapper,
+    });
     this.container = new Container({ tag: props.containerTag, class: classes.content });
     this.footer = new Footer();
   }
@@ -26,6 +36,11 @@ export default class ContentPage extends BasePage {
   render = () => {
     super.render();
     document.body.classList.add(classes.contentPage);
-    document.body.append(this.header.node, this.container.node, this.footer.node);
+    document.body.append(
+      this.header.node,
+      this.placeForBanner.node,
+      this.container.node,
+      this.footer.node,
+    );
   };
 }
