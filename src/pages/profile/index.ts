78af1694b2f36d9864a36cc2a191530fa091986a
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-05-24 11:51:03 +0200

    feat: add profile page title component

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,10 +1,13 @@
 import BaseElement from '@Src/components/common/base-element';
 import ContentPage from '@Src/components/common/content-page';
 import tag from '@Src/components/common/tag';
+import userProfileLogo from '@Assets/icons/profile-icon-dark.svg';
 import classes from './style.module.scss';
 
 export default class ProfilePage extends ContentPage {
   #content!: BaseElement<HTMLDivElement>;
+
+  titleWrapper!: BaseElement<HTMLDivElement>;
 
   constructor() {
     super({ containerTag: 'main', title: 'profile page' });
@@ -18,8 +21,25 @@ export default class ProfilePage extends ContentPage {
         tag: 'main',
         class: classes.profile,
       },
-      tag<HTMLHeadingElement>({ tag: 'h1', text: this.title }),
+      this.createTitleComponent(),
     );
+  };
+
+  createTitleComponent = () => {
+    this.titleWrapper = new BaseElement<HTMLDivElement>(
+      { tag: 'div', class: classes.titleWrapper },
+      new BaseElement<HTMLImageElement>({
+        tag: 'img',
+        src: userProfileLogo,
+        alt: 'User profile logo',
+      }),
+      new BaseElement<HTMLHeadingElement>({
+        tag: 'h1',
+        class: classes.title,
+        text: 'Your profile',
+      }),
+    );
+    return this.titleWrapper;
   };
 
   #showContent = () => {
