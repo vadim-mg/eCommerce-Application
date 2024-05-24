257e2b373c4dd729135dbb477b3579fec44d2c7b
vadim-mg <76084755+vadim-mg@users.noreply.github.com>
2024-05-24 19:53:19 +0500

    165 rss ecomm refactor before hard work) (#185)
    * feat: add getProducts function
    
    * feat: controller for products
    
    * feat: implement simple product card
    
    * feat: add base structure for catalogue page
    
    * feat: add example info to product page
    
    * feat: add error404 for not founded product
    
    * feat: add component categoryList
    
    * feat: add component productList
    
    * feat: add test data for develope
    
    * refactor: catalogue page

--- src/pages/catalogue/index.ts
+++ src/pages/catalogue/index.ts
@@ -1,17 +1,14 @@
-import categoriesApi from '@Src/api/categories';
 import BaseElement from '@Src/components/common/base-element';
 import ContentPage from '@Src/components/common/content-page';
 import tag from '@Src/components/common/tag';
-import ProductCard from '@Src/components/logic/product-card';
-import Products from '@Src/controllers/products';
+import CategoryList from '@Src/components/logic/category-list';
+import ProductList from '@Src/components/logic/product-list';
 import classes from './style.module.scss';
 
 export default class CataloguePage extends ContentPage {
   #content!: BaseElement<HTMLDivElement>;
 
-  #products: Products;
-
-  #productList!: BaseElement<HTMLDivElement>;
+  #productList!: ProductList;
 
   #filters!: BaseElement<HTMLDivElement>;
 
@@ -19,42 +16,9 @@ export default class CataloguePage extends ContentPage {
 
   constructor() {
     super({ containerTag: 'main', title: 'catalogue page' });
-    this.#products = new Products();
     this.#createContent();
-    this.#showCategories();
-    this.#showProducts();
     this.container.node.append(this.#content.node);
   }
-
-  #showProducts = async () => {
-    try {
-      const respBody = await this.#products.all;
-      this.#productList.node.innerHTML = '';
-      // console.log(respBody);
-      respBody.results.forEach((product) => {
-        console.log(product);
-        this.#productList.node.append(new ProductCard({}, product).node);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  #showCategories = async () => {
-    try {
-      const resp = await categoriesApi.getCategories();
-      const categoryList = resp.body.results.map((category) => category.name['en-GB']);
-      console.log(resp.body.results);
-      this.#categorySection.node.append(
-        tag({ tag: 'ul', text: `categories` }).node,
-        ...categoryList.map(
-          (categoryName: string) => tag({ tag: 'li', text: `${categoryName}` }).node,
-        ),
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   #createContent = () => {
     this.#content = tag<HTMLDivElement>(
@@ -62,16 +26,23 @@ export default class CataloguePage extends ContentPage {
         tag: 'main',
         class: classes.catalogue,
       },
-      (this.#categorySection = tag<HTMLDivElement>({ tag: 'div' })),
-      tag<HTMLHeadingElement>({ tag: 'h1', text: this.title }),
-      tag<HTMLDivElement>(
+
+      // category list
+      (this.#categorySection = new CategoryList({ class: classes.categories })),
+      // header
+      tag({ tag: 'h1', text: 'All games', class: classes.header }),
+
+      // main block
+      tag(
         { tag: 'div', class: classes.contentSection },
+        // filters
         (this.#filters = tag<HTMLDivElement>({
           tag: 'div',
           text: 'filters',
           class: classes.filters,
         })),
-        (this.#productList = tag<HTMLDivElement>({ tag: 'div' })),
+        // products
+        (this.#productList = new ProductList({ class: classes.products })),
       ),
     );
   };
