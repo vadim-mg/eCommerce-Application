66666f99f26bcfb177c71d22148da0fc5b3e6c13
vadim-mg <vadikm1981@yandex.ru>
2024-05-27 09:03:14 +0500

    feat: implement categories component

--- src/components/logic/category-list/index.ts
+++ src/components/logic/category-list/index.ts
@@ -1,38 +1,85 @@
 import BaseElement, { ElementProps } from '@Src/components/common/base-element';
 
 import { Category } from '@commercetools/platform-sdk';
-import categoriesApi from '@Src/api/categories';
 import tag from '@Src/components/common/tag';
+import Button, { ButtonClasses } from '@Src/components/ui/button';
+import productCategories from '@Src/controllers/categories';
 import classes from './style.module.scss';
 
 type CategoryListProps = Omit<ElementProps<HTMLLinkElement>, 'tag'>;
 
+type SimpleCategory = Pick<Category, 'id' | 'name'>;
 export default class CategoryList extends BaseElement<HTMLDivElement> {
-  #categories!: Category[];
+  #categories!: [...SimpleCategory[]];
 
-  constructor(props: CategoryListProps) {
+  #currentCategoryId: string; // if empty string, then current select is all categories
+
+  #onSelectCb: (id: string) => void;
+
+  #allButtons!: BaseElement<HTMLElement>[];
+
+  constructor(props: CategoryListProps, onSelectCb: (id: string) => void, currentCategoryId = '') {
     super({ tag: 'div', ...props });
-    this.node.classList.add(classes.categoryList);
-    this.#showCategoryes();
+    this.#onSelectCb = onSelectCb;
+    this.#currentCategoryId = currentCategoryId;
+    this.node.classList.add(classes.categoryContainer);
+
+    this.#showCategories();
   }
 
-  #showCategoryes = async () => {
+  #showCategories = async () => {
     try {
-      const resp = await categoriesApi.getCategories();
-      this.#categories = resp.body.results;
-      console.log(resp.body.results);
+      const allCategories = {
+        name: { [process.env.LOCALE]: 'All games' },
+        id: '', // for all games id = undefined or ''
+      };
+
+      this.#categories = [allCategories, ...(await productCategories.getCategories())];
+      console.log('this.#categories');
+      console.log(this.#categories);
+
+      this.#allButtons = this.#categories.map(
+        (category) =>
+          new Button(
+            {
+              text: `${category.name[process.env.LOCALE]}`,
+              id: category.id,
+            },
+            [
+              ButtonClasses.CATEGORY,
+              ...(category.id === this.#currentCategoryId ? [ButtonClasses.CURRENT_CATEGORY] : []),
+            ],
+            this.#onSelectCategory,
+          ),
+      );
+
       this.node.append(
-        tag({ tag: 'ul', text: `categories` }).node,
-        ...this.#categories.map(
-          (category) =>
-            tag({
-              tag: 'li',
-              text: `${category.name['en-GB']}`,
-            }).node,
-        ),
+        tag(
+          { tag: 'ul', class: classes.categoryList },
+          ...this.#allButtons.map((categoryButton) =>
+            tag(
+              {
+                tag: 'li',
+                class: classes.categoryItem,
+              },
+              categoryButton,
+            ),
+          ),
+        ).node,
       );
     } catch (error) {
       console.log(error);
     }
+  };
+
+  #onSelectCategory = (event: Event) => {
+    const eventTarget = event.target as HTMLButtonElement;
+    this.#onSelectCb(eventTarget.id);
+    this.#currentCategoryId = eventTarget.id;
+    console.log(`#onSelectCategory --- currentCategoryId=${this.#currentCategoryId}`);
+    this.#allButtons.forEach((button) => {
+      const btn = button as Button;
+      (btn as Button).currentStatus = btn.node.id === this.#currentCategoryId;
+    });
   };
 }
