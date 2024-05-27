0a2525a981f55d6b4f84b008042e8ce5b8e94c3d
vadim-mg <vadikm1981@yandex.ru>
2024-05-27 08:56:34 +0500

    feat: add getById for Categories

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,12 +1,14 @@
 import categoriesApi from '@Src/api/categories';
 import { Category } from '@commercetools/platform-sdk';
 
-
 class ProductCategories {
   #cache: Category[];
 
+  #map: Map<string, Category>;
+
   constructor() {
     this.#cache = [];
+    this.#map = new Map();
   }
 
   getCategories = async (fromCache = true) => {
@@ -16,11 +18,14 @@ class ProductCategories {
 
     try {
       this.#cache = (await categoriesApi.getCategories()).body.results;
+      this.#map = new Map(this.#cache.map((category) => [category.id, category]));
     } catch (error) {
       console.log(error);
     }
     return this.#cache;
   };
+
+  getById = (id: string) => this.#map.get(id);
 }
 
 const productCategories = new ProductCategories();
