65c249c222bd85fe0ab7d2cb923c41074d23caf2
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-05-27 20:12:52 +0200

    feat: make class properties private

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -9,65 +9,56 @@ import auth from '@Src/controllers/auth';
 import { HttpErrorType } from '@commercetools/sdk-client-v2';
 import classes from './style.module.scss';
 
-// interface Address {
-//   additionalAddressInfo: string,
-//   city: string,
-//   country: string,
-//   id: string,
-//   postalCode: string,
-//   streetName: string,
-// }
-
 export default class ProfilePage extends ContentPage {
   #content!: BaseElement<HTMLDivElement>;
 
-  titleWrapper!: BaseElement<HTMLDivElement>;
+  #titleWrapper!: BaseElement<HTMLDivElement>;
 
-  userDataWrapper!: BaseElement<HTMLDivElement>;
+  #userDataWrapper!: BaseElement<HTMLDivElement>;
 
-  userDataDetailsWrapper!: BaseElement<HTMLDivElement>;
+  #userDataDetailsWrapper!: BaseElement<HTMLDivElement>;
 
-  userPasswordWrapper!: BaseElement<HTMLDivElement>;
+  #userPasswordWrapper!: BaseElement<HTMLDivElement>;
 
-  editDeleteBtnWrapper!: BaseElement<HTMLDivElement>;
+  #editDeleteBtnWrapper!: BaseElement<HTMLDivElement>;
 
-  passwordInput!: InputText;
+  #passwordInput!: InputText;
 
-  passwordInputRepeat!: InputText;
+  #passwordInputRepeat!: InputText;
 
-  savePasswordButton!: Button;
+  #savePasswordButton!: Button;
 
-  userDataDetailsTitle!: BaseElement<HTMLHeadingElement>;
+  #userDataDetailsTitle!: BaseElement<HTMLHeadingElement>;
 
-  emailInput!: InputText;
+  #emailInput!: InputText;
 
-  firstNameInput!: InputText;
+  #firstNameInput!: InputText;
 
-  lastNameInput!: InputText;
+  #lastNameInput!: InputText;
 
-  birthDateInput!: InputText;
+  #birthDateInput!: InputText;
 
-  editDetailsBtn!: Button;
+  #editDetailsBtn!: Button;
 
-  saveDetailsBtn!: Button;
+  #saveDetailsBtn!: Button;
 
-  deliveryAddressesContainer!: BaseElement<HTMLDivElement>;
+  #deliveryAddressesContainer!: BaseElement<HTMLDivElement>;
 
-  billingAddressesContainer!: BaseElement<HTMLDivElement>;
+  #billingAddressesContainer!: BaseElement<HTMLDivElement>;
 
-  addAddressBtn!: Button;
+  #addAddressBtn!: Button;
 
-  editButton!: Button;
+  #editButton!: Button;
 
-  deleteButton!: Button;
+  #deleteButton!: Button;
 
-  countryInput!: InputText;
+  #countryInput!: InputText;
 
-  cityInput!: InputText;
+  #cityInput!: InputText;
 
-  postalCodeInput!: InputText;
+  #postalCodeInput!: InputText;
 
-  streetInput!: InputText;
+  #streetInput!: InputText;
 
   constructor() {
     super({ containerTag: 'main', title: 'profile page' });
@@ -81,10 +72,10 @@ export default class ProfilePage extends ContentPage {
     auth
       .me()
       .then((info) => {
-        this.emailInput.value = info.body.email ?? '';
-        this.firstNameInput.value = info.body.firstName ?? '';
-        this.lastNameInput.value = info.body.lastName ?? '';
-        this.birthDateInput.value = info.body.dateOfBirth?.split('-').join('.') ?? '';
+        this.#emailInput.value = info.body.email ?? '';
+        this.#firstNameInput.value = info.body.firstName ?? '';
+        this.#lastNameInput.value = info.body.lastName ?? '';
+        this.#birthDateInput.value = info.body.dateOfBirth?.split('-').join('.') ?? '';
 
         info.body.addresses.forEach((address) => {
           if (info.body.billingAddressIds?.includes(address.id ?? '')) {
@@ -126,7 +117,7 @@ export default class ProfilePage extends ContentPage {
   };
 
   createTitleComponent = () => {
-    this.titleWrapper = new BaseElement<HTMLDivElement>(
+    this.#titleWrapper = new BaseElement<HTMLDivElement>(
       { tag: 'div', class: classes.titleWrapper },
       new BaseElement<HTMLImageElement>({
         tag: 'img',
@@ -139,30 +130,30 @@ export default class ProfilePage extends ContentPage {
         text: 'Your profile',
       }),
     );
-    return this.titleWrapper;
+    return this.#titleWrapper;
   };
 
   createEditDeleteBtnComponent = () => {
-    this.editDeleteBtnWrapper = new BaseElement<HTMLDivElement>(
+    this.#editDeleteBtnWrapper = new BaseElement<HTMLDivElement>(
       { tag: 'div', class: classes.editDeleteBtnWrapper },
-      (this.editButton = new Button(
+      (this.#editButton = new Button(
         { text: 'Edit', class: classes.button },
         ButtonClasses.NORMAL,
         () => console.log('Edit'),
       )),
-      (this.deleteButton = new Button(
+      (this.#deleteButton = new Button(
         { text: 'Delete', class: classes.button },
         ButtonClasses.NORMAL,
         () => console.log('Delete'),
       )),
     );
-    this.editButton.node.classList.add(classes.btnLineHeight);
-    this.deleteButton.node.classList.add(classes.btnLineHeight);
-    return this.editDeleteBtnWrapper;
+    this.#editButton.node.classList.add(classes.btnLineHeight);
+    this.#deleteButton.node.classList.add(classes.btnLineHeight);
+    return this.#editDeleteBtnWrapper;
   };
 
   createUserDataComponent = () => {
-    this.userDataWrapper = new BaseElement<HTMLDivElement>(
+    this.#userDataWrapper = new BaseElement<HTMLDivElement>(
       {
         tag: 'div',
         class: classes.userDataWrapper,
@@ -170,83 +161,83 @@ export default class ProfilePage extends ContentPage {
       this.createUserDataDetailsComponent(),
       this.createUserPasswordComponent(),
     );
-    return this.userDataWrapper;
+    return this.#userDataWrapper;
   };
 
   toggleUserDetailsInputsState = (state: boolean) => {
-    this.emailInput.setDisabled(state);
-    this.firstNameInput.setDisabled(state);
-    this.lastNameInput.setDisabled(state);
-    this.birthDateInput.setDisabled(state);
+    this.#emailInput.setDisabled(state);
+    this.#firstNameInput.setDisabled(state);
+    this.#lastNameInput.setDisabled(state);
+    this.#birthDateInput.setDisabled(state);
   };
 
   toggleUserAddressInputsState = (state: boolean) => {
-    this.countryInput.setDisabled(state);
-    this.cityInput.setDisabled(state);
-    this.postalCodeInput.setDisabled(state);
-    this.streetInput.setDisabled(state);
+    this.#countryInput.setDisabled(state);
+    this.#cityInput.setDisabled(state);
+    this.#postalCodeInput.setDisabled(state);
+    this.#streetInput.setDisabled(state);
   };
 
   setEditMode = () => {
     this.toggleUserDetailsInputsState(false);
 
-    this.birthDateInput.addDateInputType();
+    this.#birthDateInput.addDateInputType();
 
-    this.editDetailsBtn.hide();
-    this.saveDetailsBtn.show();
+    this.#editDetailsBtn.hide();
+    this.#saveDetailsBtn.show();
   };
 
   setSavedMode = () => {
     this.toggleUserDetailsInputsState(true);
 
-    this.birthDateInput.addTextInputType();
+    this.#birthDateInput.addTextInputType();
 
-    this.editDetailsBtn.show();
-    this.saveDetailsBtn.hide();
+    this.#editDetailsBtn.show();
+    this.#saveDetailsBtn.hide();
   };
 
   createUserDataDetailsComponent = () => {
-    this.userDataDetailsWrapper = new BaseElement<HTMLDivElement>(
+    this.#userDataDetailsWrapper = new BaseElement<HTMLDivElement>(
       { tag: 'div', class: classes.userDataDetailsWrapper },
-      (this.userDataDetailsTitle = new BaseElement<HTMLHeadingElement>({
+      (this.#userDataDetailsTitle = new BaseElement<HTMLHeadingElement>({
         tag: 'h2',
         text: 'Personal details',
       })),
-      (this.emailInput = new InputText({ name: 'email', type: 'email' }, 'E-mail')),
-      (this.firstNameInput = new InputText({ name: 'firstName' }, 'Fist name')),
-      (this.lastNameInput = new InputText({ name: 'lastName' }, 'Last name')),
-      (this.birthDateInput = new InputText({ name: 'date-of-birth' }, 'Birth date')),
-      (this.editDetailsBtn = new Button(
+      (this.#emailInput = new InputText({ name: 'email', type: 'email' }, 'E-mail')),
+      (this.#firstNameInput = new InputText({ name: 'firstName' }, 'Fist name')),
+      (this.#lastNameInput = new InputText({ name: 'lastName' }, 'Last name')),
+      (this.#birthDateInput = new InputText({ name: 'date-of-birth' }, 'Birth date')),
+      (this.#editDetailsBtn = new Button(
         { text: 'Edit details', class: classes.btnLineHeight },
         ButtonClasses.NORMAL,
         this.setEditMode,
       )),
-      (this.saveDetailsBtn = new Button(
+      (this.#saveDetailsBtn = new Button(
         { text: 'Save', class: classes.btnLineHeight },
         ButtonClasses.NORMAL,
         this.setSavedMode,
       )),
     );
-    this.birthDateInput.node.classList.add(classes.birthDateInput);
+    this.#birthDateInput.node.classList.add(classes.birthDateInput);
 
     this.toggleUserDetailsInputsState(true);
-    this.saveDetailsBtn.hide();
+    this.#saveDetailsBtn.hide();
 
-    return this.userDataDetailsWrapper;
+    return this.#userDataDetailsWrapper;
   };
 
   createUserPasswordComponent = () => {
-    this.userPasswordWrapper = new BaseElement<HTMLDivElement>(
+    this.#userPasswordWrapper = new BaseElement<HTMLDivElement>(
       {
         tag: 'div',
         class: classes.userPasswordWrapper,
       },
       new BaseElement<HTMLHeadingElement>({ tag: 'h2', text: 'Change password' }),
-      (this.passwordInput = new InputText(
+      (this.#passwordInput = new InputText(
         { name: 'password', maxLength: 20, minLength: 8, type: 'password' },
         'New password',
       )),
-      (this.passwordInputRepeat = new InputText(
+      (this.#passwordInputRepeat = new InputText(
         { name: 'password', maxLength: 20, minLength: 8, type: 'password' },
         'Repeat password',
       )),
@@ -255,16 +246,16 @@ export default class ProfilePage extends ContentPage {
         class: classes.passwordRule,
         text: '! The password must be at least 8 characters long. It must contain Latin letters, at least one digit and at least one capital letter.',
       }),
-      (this.savePasswordButton = new Button(
+      (this.#savePasswordButton = new Button(
         { text: 'Save password', class: classes.button },
         ButtonClasses.NORMAL,
         () => console.log('Save password'),
       )),
     );
-    this.passwordInput.node.classList.add(classes.inputMargin);
-    this.passwordInputRepeat.node.classList.add(classes.inputMargin);
-    this.savePasswordButton.node.classList.add(classes.btnLineHeight);
-    return this.userPasswordWrapper;
+    this.#passwordInput.node.classList.add(classes.inputMargin);
+    this.#passwordInputRepeat.node.classList.add(classes.inputMargin);
+    this.#savePasswordButton.node.classList.add(classes.btnLineHeight);
+    return this.#userPasswordWrapper;
   };
 
   createAddressComponent = (
@@ -276,25 +267,25 @@ export default class ProfilePage extends ContentPage {
   ) => {
     const addressComponent = new BaseElement<HTMLDivElement>(
       { tag: 'div', class: classes.addressWrapper },
-      (this.countryInput = new InputText({ name: 'country' }, 'Country')),
-      (this.cityInput = new InputText({ name: 'city' }, 'City')),
-      (this.postalCodeInput = new InputText({ name: 'postal code' }, 'Postal code')),
-      (this.streetInput = new InputText({ name: 'street' }, 'Street')),
+      (this.#countryInput = new InputText({ name: 'country' }, 'Country')),
+      (this.#cityInput = new InputText({ name: 'city' }, 'City')),
+      (this.#postalCodeInput = new InputText({ name: 'postal code' }, 'Postal code')),
+      (this.#streetInput = new InputText({ name: 'street' }, 'Street')),
       // todo: change 3rd argument later
       new CheckBox({ class: classes.checkbox }, `Use us default ${addressType} address`, true),
       this.createEditDeleteBtnComponent(),
     );
-    this.countryInput.value = country;
-    this.cityInput.value = city;
-    this.postalCodeInput.value = postalCode;
-    this.streetInput.value = street;
+    this.#countryInput.value = country;
+    this.#cityInput.value = city;
+    this.#postalCodeInput.value = postalCode;
+    this.#streetInput.value = street;
 
     this.toggleUserAddressInputsState(true);
 
     if (addressType === 'billing') {
-      this.billingAddressesContainer.node.append(addressComponent.node);
+      this.#billingAddressesContainer.node.append(addressComponent.node);
     } else {
-      this.deliveryAddressesContainer.node.append(addressComponent.node);
+      this.#deliveryAddressesContainer.node.append(addressComponent.node);
     }
     return addressComponent;
   };
@@ -304,11 +295,11 @@ export default class ProfilePage extends ContentPage {
       tag: 'h2',
       text: 'Delivery address',
     });
-    this.addAddressBtn = this.createAddAddressBtn();
-    this.deliveryAddressesContainer = new BaseElement<HTMLDivElement>({ tag: 'div' });
-    this.deliveryAddressesContainer.node.append(deliveryAddressTitle.node);
-    this.deliveryAddressesContainer.node.append(this.addAddressBtn.node);
-    return this.deliveryAddressesContainer;
+    this.#addAddressBtn = this.createAddAddressBtn();
+    this.#deliveryAddressesContainer = new BaseElement<HTMLDivElement>({ tag: 'div' });
+    this.#deliveryAddressesContainer.node.append(deliveryAddressTitle.node);
+    this.#deliveryAddressesContainer.node.append(this.#addAddressBtn.node);
+    return this.#deliveryAddressesContainer;
   };
 
   createBillingAddressBasicStructure = () => {
@@ -316,21 +307,21 @@ export default class ProfilePage extends ContentPage {
       tag: 'h2',
       text: 'Billing address',
     });
-    this.addAddressBtn = this.createAddAddressBtn();
-    this.billingAddressesContainer = new BaseElement<HTMLDivElement>({ tag: 'div' });
-    this.billingAddressesContainer.node.append(billingAddressTitle.node);
-    this.billingAddressesContainer.node.append(this.addAddressBtn.node);
-    return this.billingAddressesContainer;
+    this.#addAddressBtn = this.createAddAddressBtn();
+    this.#billingAddressesContainer = new BaseElement<HTMLDivElement>({ tag: 'div' });
+    this.#billingAddressesContainer.node.append(billingAddressTitle.node);
+    this.#billingAddressesContainer.node.append(this.#addAddressBtn.node);
+    return this.#billingAddressesContainer;
   };
 
   createAddAddressBtn = () => {
-    this.addAddressBtn = new Button(
+    this.#addAddressBtn = new Button(
       { text: '+Add address', class: classes.button },
       ButtonClasses.NORMAL,
       () => console.log('Add address'),
     );
-    this.addAddressBtn.node.classList.add(classes.addAddressBtn);
-    return this.addAddressBtn;
+    this.#addAddressBtn.node.classList.add(classes.addAddressBtn);
+    return this.#addAddressBtn;
   };
 
   #showContent = () => {
