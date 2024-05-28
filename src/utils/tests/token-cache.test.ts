8d00cc42c95fd231fa36226e3954fb96c33d155e
vadim-mg <vadikm1981@yandex.ru>
2024-05-28 10:02:08 +0500

    refactor: rename wrong var in tests

--- src/utils/tests/token-cache.test.ts
+++ src/utils/tests/token-cache.test.ts
@@ -10,18 +10,18 @@ const savedRealToken = anonymousTokenCache.get();
 describe('token cache', () => {
   test('set and get works right', () => {
     anonymousTokenCache.set({ token, refreshToken, expirationTime });
-    const gettedToken = anonymousTokenCache.get();
-    expect(gettedToken.token).toBe(token);
-    expect(gettedToken.refreshToken).toBe(refreshToken);
-    expect(gettedToken.expirationTime).toBe(expirationTime);
+    const gottenToken = anonymousTokenCache.get();
+    expect(gottenToken.token).toBe(token);
+    expect(gottenToken.refreshToken).toBe(refreshToken);
+    expect(gottenToken.expirationTime).toBe(expirationTime);
   });
 
   test('remove works right', () => {
     anonymousTokenCache.remove();
-    const gettedToken = anonymousTokenCache.get();
-    expect(gettedToken.token).toBe(undefined);
-    expect(gettedToken.refreshToken).toBe(undefined);
-    expect(gettedToken.expirationTime).toBe(undefined);
+    const gottenToken = anonymousTokenCache.get();
+    expect(gottenToken.token).toBe(undefined);
+    expect(gottenToken.refreshToken).toBe(undefined);
+    expect(gottenToken.expirationTime).toBe(undefined);
   });
 });
 
