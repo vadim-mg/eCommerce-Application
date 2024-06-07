d21c627722a55044b09cd37dd308775845f138fa
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-06-07 14:12:31 +0200

    feat: create team members component

--- src/pages/about/index.ts
+++ src/pages/about/index.ts
@@ -1,6 +1,10 @@
 import BaseElement from '@Src/components/common/base-element';
 import ContentPage from '@Src/components/common/content-page';
 import tag from '@Src/components/common/tag';
+import TeamMember from '@Src/components/ui/team-member';
+import vadimPhoto from '@Assets/img/vadim.jpg';
+import yaninaPhoto from '@Assets/img/yanina.jpg';
+import natalyaPhoto from '@Assets/img/natalya.jpg';
 import classes from './style.module.scss';
 
 export default class AboutPage extends ContentPage {
@@ -18,7 +22,43 @@ export default class AboutPage extends ContentPage {
         tag: 'main',
         class: classes.about,
       },
-      tag<HTMLHeadingElement>({ tag: 'h1', text: this.title }),
+      tag<HTMLHeadingElement>({ tag: 'h1', text: 'About us', class: classes.mainTitle }),
+      new BaseElement<HTMLDivElement>(
+        { tag: 'div', class: classes.teamMembersWrapper },
+        new TeamMember(
+          'Vadim',
+          'TeamLead',
+          'Russia, Tyumen',
+          'higher education TGNGU',
+          'RSScool: NodeJs, Frontend (stage0, stage1, stage2)',
+          'I like to learn new things, at this stage it is front-end development. Hobbies: tourism and many others)',
+          'vadim-mg',
+          'https://github.com/vadim-mg',
+          vadimPhoto,
+        ),
+        new TeamMember(
+          'Yanina',
+          'frontend, tester',
+          'Poland, Wroclaw',
+          'VSMU, Pharmaceutical faculty',
+          'RSScool Frontend (stage1, stage2)',
+          'I strive to become a better version of myself. Hobbies: board games, piano, bouldering',
+          'YanaLysukha',
+          'https://github.com/YanaLysukha',
+          yaninaPhoto,
+        ),
+        new TeamMember(
+          'Natalya',
+          'frontend, ui designer',
+          'Russia, Vladimir',
+          'vocational education VPC',
+          'RSScool/Frontend: stage0, stage1, stage2, TGNGU: UI designer',
+          'I love interfaces. Thinking through the user path, drawing and implementing. Hobbies: juggling, fire show.',
+          'Nuttik',
+          'https://github.com/Nuttik',
+          natalyaPhoto,
+        ),
+      ),
     );
   };
 
