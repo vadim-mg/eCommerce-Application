358c9347cb611ed97388c6e57771e3c133aa5afa
vadim-mg <vadikm1981@yandex.ru>
2024-06-07 10:34:21 +0500

    feat: add base api methods for cart

---
+++ src/api/cart.ts
@@ -0,0 +1,28 @@
+import { MyCartDraft, MyCartUpdateAction } from '@commercetools/platform-sdk';
+import apiRoot from './api-root';
+
+const getActiveCart = () => apiRoot.apiBuilder.me().activeCart().get().execute();
+
+const createCart = (myCartDraft: MyCartDraft) =>
+  apiRoot.apiBuilder
+    .me()
+    .carts()
+    .post({
+      body: myCartDraft,
+    })
+    .execute();
+
+const updateCart = (ID: string, version: number, actions: MyCartUpdateAction[]) =>
+  apiRoot.apiBuilder
+    .me()
+    .carts()
+    .withId({ ID })
+    .post({
+      body: {
+        version,
+        actions,
+      },
+    })
+    .execute();
+
+export default { getActiveCart, createCart, updateCart };
