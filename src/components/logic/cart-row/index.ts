017b7a88d9a7819c4a3ec3026da2958c252ea3d5
Natalia Repkina <41197830+Nuttik@users.noreply.github.com>
2024-06-11 18:28:29 +0300

    Feat/modify the quantity and remove from cart (#250)
    * feat: add message for empty cart
    
    * feat: implement remove product from cart
    
    * feat: implement change in product quantity with API
    
    * feat: add refresh count products in header element
    
    * refactor: separated logic from the view and fixed product deletion
    
    * refactor: remove unused methods
    
    * refactor: add try catch in methods with API
    
    * refactor: add comma
    
    * fix: disable the plus and minus buttons until the server responds
    
    * feat: add a link to the items in the cart
    
    * refactor: format prettier
    
    * fix: remove setTimeout from spinner-input
    
    * refactor: clean up the class name mistake SpinnerInput
    
    * fix: link in cart-row
    
    * refactor: format

---
+++ src/components/logic/cart-row/index.ts
@@ -0,0 +1,124 @@
+import crossSVG from '@Assets/icons/cross.svg';
+import trashSVG from '@Assets/icons/trash.svg';
+import BaseElement from '@Src/components/common/base-element';
+import tag from '@Src/components/common/tag';
+import Link from '@Src/components/ui/link';
+import SpinnerInput from '@Src/components/ui/spinner-input';
+import cartController from '@Src/controllers/cart';
+import Products, { ImageSize } from '@Src/controllers/products';
+import { AppRoutes } from '@Src/router/routes';
+import { LineItem } from '@commercetools/platform-sdk';
+import classes from './style.module.scss';
+
+export default class CartRow extends BaseElement<HTMLElement> {
+  #onChangeProductStateInCart: () => void;
+
+  constructor(onChangeProductStateInCart: () => void, dataItem: LineItem) {
+    super({
+      tag: 'div',
+      class:
+        dataItem.totalPrice.centAmount / dataItem.quantity !== dataItem.price.value.centAmount
+          ? [classes.prodRow, classes.prodRowDiscount]
+          : classes.prodRow,
+    });
+    this.#onChangeProductStateInCart = onChangeProductStateInCart;
+    // image
+    const imgEl = tag(
+      { tag: 'div', class: classes.prodRowImgWrapper },
+      tag<HTMLImageElement>({
+        tag: 'img',
+        class: classes.prodRowImg,
+        src: Products.getImageUrl(
+          dataItem.variant.images ? dataItem.variant.images[0].url : '' ?? '',
+          ImageSize.small,
+        ),
+        alt: dataItem.name[Products.locale],
+      }),
+    );
+    // name
+    console.log();
+    const nameEl = new Link({
+      class: classes.prodRowName,
+      text: dataItem.name[Products.locale],
+      href: `${AppRoutes.CATALOGUE}/all/${dataItem.productKey}`,
+    });
+    const rightPart = tag(
+      { tag: 'div', class: classes.prodRowRight },
+      // block with price for one and quantity
+      tag(
+        { tag: 'div', class: classes.prodRowPricesAndCount },
+        tag(
+          { tag: 'div', class: classes.prodRowPrices },
+          // normal price
+          tag({
+            tag: 'div',
+            class:
+              dataItem.totalPrice.centAmount / dataItem.quantity !==
+              dataItem.price.value.centAmount
+                ? classes.prodRowPriceOld
+                : classes.prodRowPrice,
+            innerHTML: `€${(dataItem.price.value.centAmount / 100).toFixed(2)}`,
+          }),
+          // discount price
+          tag({
+            tag: 'div',
+            class: classes.prodRowPrice,
+            innerHTML:
+              dataItem.totalPrice.centAmount / dataItem.quantity !==
+              dataItem.price.value.centAmount
+                ? `€${(dataItem.totalPrice.centAmount / 100 / dataItem.quantity).toFixed(2)}`
+                : '',
+          }),
+        ),
+        // cross icon
+        tag({ tag: 'div', class: classes.prodRowCross, innerHTML: crossSVG }),
+
+        new SpinnerInput(
+          dataItem.quantity,
+          classes.spinnerInput,
+          this.#onPlusHandler.bind(this, String(dataItem.productId)),
+          this.#onMinusHandler.bind(this, String(dataItem.productId)),
+        ),
+      ),
+      // total price
+      tag({
+        tag: 'div',
+        class: classes.prodRowTotalPrice,
+        text: `€${(dataItem.totalPrice.centAmount / 100).toFixed(2)}`,
+      }),
+      // trash icon
+      tag({
+        tag: 'div',
+        class: classes.prodRowTrash,
+        innerHTML: trashSVG,
+        onclick: async () => {
+          try {
+            await cartController.removeItemFromCart(String(dataItem.productId), dataItem.quantity);
+            onChangeProductStateInCart();
+          } catch (e) {
+            console.log(e);
+          }
+        },
+      }),
+    );
+    this.node.append(imgEl.node, nameEl.node, rightPart.node);
+  }
+
+  #onMinusHandler = async (prodId: string) => {
+    try {
+      await cartController.removeItemFromCart(prodId, 1);
+      this.#onChangeProductStateInCart();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  #onPlusHandler = async (prodId: string) => {
+    try {
+      await cartController.addItemToCart(prodId);
+      this.#onChangeProductStateInCart();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
--- src/components/ui/spinner-input/index.ts
+++ src/components/ui/spinner-input/index.ts
@@ -2,14 +2,24 @@ import BaseElement from '@Src/components/common/base-element';
 import tag from '@Src/components/common/tag';
 import classes from './style.module.scss';
 
-export default class SpinerInput extends BaseElement<HTMLElement> {
+export type CallbackSpinner = () => Promise<void>;
+export default class SpinnerInput extends BaseElement<HTMLElement> {
   #input: BaseElement<HTMLInputElement>;
 
   #minus: BaseElement<HTMLElement>;
 
   #plus: BaseElement<HTMLElement>;
 
-  constructor(quantity: number, className: string, callback: () => void) {
+  #callbackPlus: CallbackSpinner;
+
+  #callbackMinus: CallbackSpinner;
+
+  constructor(
+    quantity: number,
+    className: string,
+    callbackPlus: CallbackSpinner,
+    callbackMinus: CallbackSpinner,
+  ) {
     super({ tag: 'div', class: classes.wrapper });
     this.node.classList.add(className);
 
@@ -19,24 +29,29 @@ export default class SpinerInput extends BaseElement<HTMLElement> {
       type: 'number',
       value: String(quantity),
     });
+    this.#callbackPlus = callbackPlus;
+    this.#callbackMinus = callbackMinus;
     this.#minus = tag({ tag: 'div', class: classes.minus, text: '-' });
     this.#plus = tag({ tag: 'div', class: classes.plus, text: '+' });
 
     this.#minus.node.addEventListener('click', this.#minusHandler);
     this.#plus.node.addEventListener('click', this.#plusHandler);
-    this.#input.node.addEventListener('change', callback);
 
     this.#setMinus();
     this.node.append(this.#minus.node, this.#input.node, this.#plus.node);
   }
 
-  #minusHandler = () => {
+  #minusHandler = async () => {
+    this.#disableButtons();
     this.#input.node.value = String(parseInt(this.#input.node.value, 10) - 1);
+    await this.#callbackMinus();
     this.#setMinus();
   };
 
-  #plusHandler = () => {
+  #plusHandler = async () => {
+    this.#disableButtons();
     this.#input.node.value = String(parseInt(this.#input.node.value, 10) + 1);
+    await this.#callbackPlus();
     this.#setMinus();
   };
 
@@ -46,5 +61,12 @@ export default class SpinerInput extends BaseElement<HTMLElement> {
     } else {
       this.#minus.node.classList.remove(classes.disable);
     }
+  };
+
+  #disableButtons = () => {
+    if (!this.#minus.node.classList.contains(classes.disable))
+      this.#minus.node.classList.add(classes.disable);
+    if (!this.#plus.node.classList.contains(classes.disable))
+      this.#plus.node.classList.add(classes.disable);
   };
 }
